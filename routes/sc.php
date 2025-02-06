1e7268b3dc5057ad0cb37fee84ec9492337053e6
ThachVD <thachvd@thefirstone.jp>
2025-02-06 11:24:44 +0700

    update postman collection

--- routes/sc.php
+++ routes/sc.php
@@ -10,6 +10,7 @@ Route::prefix('/api')->group(function () {
     Route::prefix('/tl-lincoln')->group(function() {
         Route::post('/empty-room', [TlLincolnController::class, 'getEmptyRoom'])->name('tllincoln-api.get-empty-room');
         Route::post('/bulk-empty-room', [TlLincolnController::class, 'getBulkEmptyRoom'])->name('tllincoln-api.get-bulk-empty-room');
-        Route::post('/price-plan', [TlLincolnController::class, 'getPricePlan'])->name('tllincoln-api.get-bulk-empty-room');
+        Route::post('/price-plan', [TlLincolnController::class, 'getPricePlan'])->name('tllincoln-api.get-price-plan');
+        Route::post('/bulk-price-plan', [TlLincolnController::class, 'getBulkPricePlan'])->name('tllincoln-api.get-bulk-price-plan');
     });
 });
