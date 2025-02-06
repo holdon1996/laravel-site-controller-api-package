788660eb0e1d3207fa3b0bd71935013acfed9960
ThachVD <thachvd@thefirstone.jp>
2025-02-06 13:56:19 +0700

    add route

--- packages/thachvd/laravel-site-controller-api/src/routes/sc.php
+++ packages/thachvd/laravel-site-controller-api/src/routes/sc.php
@@ -4,7 +4,13 @@ use App\Http\Controllers\Sc\TlLincolnController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api')->group(function () {
-    Route::prefix('/tl-lincoln', function() {
+    Route::get('/health', function (){
+        echo "ok";
+    });
+    Route::prefix('/tl-lincoln')->group(function() {
         Route::post('/empty-room', [TlLincolnController::class, 'getEmptyRoom'])->name('tllincoln-api.get-empty-room');
+        Route::post('/bulk-empty-room', [TlLincolnController::class, 'getBulkEmptyRoom'])->name('tllincoln-api.get-bulk-empty-room');
+        Route::post('/price-plan', [TlLincolnController::class, 'getPricePlan'])->name('tllincoln-api.get-price-plan');
+        Route::post('/bulk-price-plan', [TlLincolnController::class, 'getBulkPricePlan'])->name('tllincoln-api.get-bulk-price-plan');
     });
 });
--- routes/sc.php
+++ routes/sc.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\Sc\TlLincolnController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api')->group(function () {
-    Route::get('', function (){
-        echo "hello";
+    Route::get('/health', function (){
+        echo "ok";
     });
     Route::prefix('/tl-lincoln')->group(function() {
         Route::post('/empty-room', [TlLincolnController::class, 'getEmptyRoom'])->name('tllincoln-api.get-empty-room');
